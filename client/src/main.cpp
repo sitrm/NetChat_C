869a3fed292e7cd400b3acb66d59b54229ebcbdb
sitrm <dmit_04bur@mail.ru>
2025-03-04 17:00:47 +0300

    add parser argc/argv

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,11 +1,34 @@
 #include "../include/clientChat.h"
 
 
+void printHelp(const std::string& programName) {
+    std::cout << "Usage: " << programName << " <IP_ADDRESS> <PORT>" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -h, --help    Show this help message and exit" << std::endl;
+}
 
-int main() {
-	int PORT_SERVER = 8888;
-	const std::string IP_SERVER = "127.0.0.1";
+
+int main(int argc, char* argv[]) {
 	
+	
+	for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--help" || arg == "-h") {
+            printHelp(argv[0]);
+            return EXIT_SUCCESS;
+        }
+    }
+
+	
+	if (argc < 3) {
+		std::cerr << "Error: Not enough arguments." << std::endl;
+		printHelp(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	const std::string IP_SERVER = argv[1];
+	int PORT_SERVER = std::stoi(argv[2]);
+
 	try{
 		NetChat::Client client(PORT_SERVER, IP_SERVER);
 		client.startSerialize();
@@ -14,8 +37,6 @@ int main() {
 		std::cerr << "Error: " << e.what() << std::endl;
 		exit(EXIT_FAILURE);
 	}
-
-
 
 	return 0;
 }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,14 +1,36 @@
 #include "../include/serverChat.h"
+#include <string>
+
+void printHelp(const std::string& programName) {
+    std::cout << "Usage: " << programName << " <IP_ADDRESS> <PORT>" << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  -h, --help    Show this help message and exit" << std::endl;
+}
 
 
+int main(int argc, char* argv[]) {
+	
+	
+	for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--help" || arg == "-h") {
+            printHelp(argv[0]);
+            return EXIT_SUCCESS;
+        }
+    }
 
-int main() {
+	
+    if (argc < 3) {
+        std::cerr << "Error: Not enough arguments." << std::endl;
+        printHelp(argv[0]);
+        return EXIT_FAILURE;
+    }
 
-	int PORT = 8888;
-	const std::string ipAddress = "127.0.0.1";
+	const std::string IP_SERVER = argv[1];
+	int PORT_SERVER = std::stoi(argv[2]);
 
-	try {
-		NetChat::Server server(PORT, ipAddress);
+	try{
+		NetChat::Server server(PORT_SERVER, IP_SERVER);
 		server.start();
 	}
 	catch (const std::exception& e) {
